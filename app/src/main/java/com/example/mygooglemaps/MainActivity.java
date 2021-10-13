4063501a8482daccaf239e7b8472c7100804f50a
Yaseen <arrahman.visualtech@gmail.com>
2021-10-13 00:51:27 -0700

    Permissions Work Done

--- app/src/main/java/com/example/mygooglemaps/MainActivity.java
+++ app/src/main/java/com/example/mygooglemaps/MainActivity.java
@@ -1,53 +1,66 @@
 package com.example.mygooglemaps;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private final int REQUEST_LOCATION_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkRunTimePermission();
+        RequestMapPermissions();
 
     }
 
-    public void checkRunTimePermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED||
-                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Granted", Toast.LENGTH_SHORT).show();
+    private void RequestMapPermissions() {
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                1);
+    }
 
-            } else {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
-                        10);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        switch (requestCode){
+
+            case 1:{
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this, "Granted", Toast.LENGTH_SHORT).show();
+                    //Here you can Continue working....
+
+                }
+                else
+                    {
+                    Toast.makeText(MainActivity.this, "Please Turn On Location First", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent.setData(uri);
+                    startActivity(intent);
+                    finish();
+                }
+
+                return;
             }
-        } else {
-            Toast.makeText(this, "Not Granted", Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, "For using this app you need to fine the location...", Toast.LENGTH_SHORT).show();
-            MainActivity.this.finishAffinity();
+
+
+            //Other Cases will came here....
+
+
         }
+
+
     }
-
-
-
 }
